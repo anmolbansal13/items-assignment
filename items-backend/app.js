46fca600674716108c1ee99b58c29891d799559e
Anmol Bansal <anmol13200159@gmail.com>
2025-06-21 06:32:20 +0530

    cors fix 5

--- items-backend/app.js
+++ items-backend/app.js
@@ -7,12 +7,15 @@ const itemRoutes = require("./routes/itemRoutes");
 const { connectDB } = require("./config/database");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+      process.env.FRONTEND_URL,
+      "https://items-assignment.vercel.app",
+      "http://localhost:3000"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
     allowedHeaders: [
       "Content-Type",
@@ -27,18 +30,15 @@ app.use(
     credentials: true,
   })
 );
-// app.options("*", cors());
-app.use(express.json({ limit: "50mb" })); // Increased limit for base64 images
+
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
-// Connect to MongoDB
 connectDB();
-// Serve uploaded files
-app.use("/uploads", express.static("uploads"));
-// Routes
 
 app.use("/items", itemRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
+// app.listen(process.env.PORT || 5000, () => {
+//   console.log(`Server is running on port ${process.env.PORT}`);
+// })
